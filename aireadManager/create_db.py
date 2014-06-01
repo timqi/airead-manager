206be5c871a6e93a916c89d373a0a5384fe1bb95
Airead Fan <fanrenhao@crazyhornets.com>
2014-06-01 22:25:15 +0800

    init user and permission

--- aireadManager/create_db.py
+++ aireadManager/create_db.py
@@ -1,10 +1,45 @@
+# coding=utf-8
+from datetime import datetime
+
 from main import app
 from main import db
 from model.user import UserModel
+from model.permission import PermissionModel
+
+
+def admin_init():
+    admin = {
+        'username': 'admin',
+        'first_name': 'admin',
+        'last_name': 'admin',
+        'email': 'admin@some.com',
+        'password': 'admin',
+        'is_staff': True,
+        'is_active': True,
+        'is_superuser': True,
+        'last_login': datetime.now(),
+        'date_joined': datetime.now()
+    }
+    user = UserModel(**admin)
+    db.session.add(user)
+    db.session.commit()
+
+
+def permission_init():
+    permissions = [
+        {'name': u'管理员', 'tag': 'admin'},
+        {'name': u'游客', 'tag': 'guest'},
+    ]
+
+    for p in permissions:
+        _ = PermissionModel(**p)
+        db.session.add(_)
+
+    db.session.commit()
+
 
 with app.app_context():
     db.create_all()
 
-    #user = UserModel('admin', 'admin')
-    #db.session.add(user)
-    #db.session.commit()
+    admin_init()
+    permission_init()
