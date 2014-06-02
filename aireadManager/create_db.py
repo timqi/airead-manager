8b5d9ed8a48a358f7c4ba2e7c61416f39e5a2569
Airead Fan <fanrenhao@crazyhornets.com>
2014-06-02 11:29:13 +0800

    update create_db

--- aireadManager/create_db.py
+++ aireadManager/create_db.py
@@ -3,8 +3,11 @@ from datetime import datetime
 
 from main import app
 from main import db
-from model.user import UserModel
-from model.permission import PermissionModel
+from aireadManager.model.user import UserModel
+from aireadManager.model.group import GroupModel
+from aireadManager.model.user_group import UserGroupModel
+from aireadManager.model.permission import PermissionModel
+from aireadManager.model.group_permission import GroupPermissionModel
 
 
 def admin_init():
@@ -20,9 +23,46 @@ def admin_init():
         'last_login': datetime.now(),
         'date_joined': datetime.now()
     }
+
+    u = db.session.query(UserModel).filter_by(username='admin').first()
+    if u:
+        return None
+
     user = UserModel(**admin)
     db.session.add(user)
     db.session.commit()
+
+
+def group_init():
+    group = {
+        'name': u'管理组'
+    }
+
+    existed = db.session.query(GroupModel).filter_by(name=group['name']).first()
+    if not existed:
+        g = GroupModel(**group)
+        db.session.add(g)
+        db.session.commit()
+
+
+def user_group_init():
+    adminUser = db.session.query(UserModel).filter_by(username='admin').first()
+    if not adminUser:
+        raise Exception('没有发现管理员用户')
+
+    adminGroup = db.session.query(GroupModel).filter_by(name=u'管理组').first()
+    if not adminGroup:
+        raise Exception('没有发现管理组')
+
+    adminUserGroup = db.session.query(UserGroupModel).filter_by(
+        user_id=adminUser.id
+    ).filter_by(
+        group_id=adminGroup.id
+    ).first()
+    if not adminUserGroup:
+        u_g = UserGroupModel(user_id=adminUser.id, group_id=adminGroup.id)
+        db.session.add(u_g)
+        db.session.commit()
 
 
 def permission_init():
@@ -32,14 +72,40 @@ def permission_init():
     ]
 
     for p in permissions:
+        existed = db.session.query(PermissionModel).filter_by(tag=p['tag']).first()
+        if existed:
+            continue
         _ = PermissionModel(**p)
         db.session.add(_)
 
     db.session.commit()
 
 
+def group_permission_init():
+    adminGroup = db.session.query(GroupModel).filter_by(name=u'管理组').first()
+    if not adminGroup:
+        raise Exception('没有发现管理组')
+
+    adminPerm = db.session.query(PermissionModel).filter_by(tag='admin').first()
+    if not adminPerm:
+        raise Exception('没有发现管理权限')
+
+    adminGroupPerm = db.session.query(GroupPermissionModel).filter_by(
+        group_id=adminGroup.id
+    ).filter_by(
+        permission_id=adminPerm.id
+    ).first()
+    if not adminGroupPerm:
+        g_p = GroupPermissionModel(group_id=adminGroup.id, permission_id=adminPerm.id)
+        db.session.add(g_p)
+        db.session.commit()
+
+
 with app.app_context():
     db.create_all()
 
     admin_init()
+    group_init()
+    user_group_init()
     permission_init()
+    group_permission_init()
